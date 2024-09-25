b5772d3044f3cb09ffcccbb778ff0e6400aebcea
David Correa Rodríguez <david.correa@wazuh.com>
2024-09-25 12:53:36 +0200

    Fixed filebeat_wazuh_template variable

--- install_functions/installVariables.sh
+++ install_functions/installVariables.sh
@@ -29,7 +29,7 @@ config_file="${base_path}/config.yml"
 readonly tar_file_name="wazuh-install-files.tar"
 tar_file="${base_path}/${tar_file_name}"
 
-filebeat_wazuh_template="https://raw.githubusercontent.com/wazuh/wazuh/v${source_branch}/extensions/elasticsearch/7.x/wazuh-template.json"
+filebeat_wazuh_template="https://raw.githubusercontent.com/wazuh/wazuh/${source_branch}/extensions/elasticsearch/7.x/wazuh-template.json"
 
 readonly dashboard_cert_path="/etc/wazuh-dashboard/certs"
 readonly filebeat_cert_path="/etc/filebeat/certs"
