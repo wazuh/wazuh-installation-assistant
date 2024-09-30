9f3c5d0ea1978e5326701891735dd352dda9f223
c-bordon <carlos.e.bordon@gmail.com>
2024-09-30 14:39:32 -0300

    Indentation fixed

--- install_functions/installCommon.sh
+++ install_functions/installCommon.sh
@@ -211,7 +211,7 @@ function installCommon_createInstallFiles() {
         eval "chown root:root /tmp/wazuh-install-files/* ${debug}"
         eval "tar -zcf '${tar_file}' -C '/tmp/' wazuh-install-files/ ${debug}"
         eval "rm -rf '/tmp/wazuh-install-files' ${debug}"
-	    eval "rm -rf ${config_file} ${debug}"
+        eval "rm -rf ${config_file} ${debug}"
         common_logger "Created ${tar_file_name}. It contains the Wazuh cluster key, certificates, and passwords necessary for installation."
     else
         common_logger -e "Unable to create /tmp/wazuh-install-files"
