f9ceb290c31cbca4a7059f96a9cd75cdee4e42e3
Carlos Anguita López <canguita99@gmail.com>
2024-11-19 12:03:00 +0100

    add support to tolerate ips in the config.yml between <> or double quotes

--- cert_tool/certFunctions.sh
+++ cert_tool/certFunctions.sh
@@ -367,8 +367,8 @@ function cert_readConfig() {
         all_ips=("${indexer_node_ips[@]}" "${server_node_ips[@]}" "${dashboard_node_ips[@]}")
 
         for ip in "${all_ips[@]}"; do
-            isIP=$(echo "${ip}" | grep -P "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$")
-            isDNS=$(echo "${ip}" | grep -P "^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])\.([A-Za-z]{2,})$" )
+            isIP=$(echo "${ip}" | grep -P "^[\"'<]*[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}[\"'>]*$")
+            isDNS=$(echo "${ip}" | grep -P "^[\"'<]*(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])\.([A-Za-z]{2,})[\"'>]*$" )
             if [[ -n "${isIP}" ]]; then
                 if ! cert_checkPrivateIp "$ip"; then
                     common_logger -e "The IP ${ip} is public."
