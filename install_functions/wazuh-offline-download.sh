4104ee2233714ed1ab703fa0d53bc697bb78ade6
Enrique Araque <minixute@gmail.com>
2024-12-17 18:38:28 +0100

    Fix staging name input and update changelog

--- install_functions/wazuh-offline-download.sh
+++ install_functions/wazuh-offline-download.sh
@@ -7,7 +7,7 @@
 # and/or modify it under the terms of the GNU General Public
 # License (version 2) as published by the FSF - Free Software
 # Foundation.
- 
+
 function offline_download() {
 
   common_logger "Starting Wazuh packages download."
