f05188602c671ae293f6ab7b9b85de1ae9ad289a
Enrique Araque <minixute@gmail.com>
2024-12-17 18:44:22 +0100

    Change Wazuh version to test workflow changes

--- install_functions/installVariables.sh
+++ install_functions/installVariables.sh
@@ -8,7 +8,7 @@
 
 ## Package vars
 readonly wazuh_major="4.10"
-readonly wazuh_version="4.10.2"
+readonly wazuh_version="4.10.0"
 readonly filebeat_version="7.10.2"
 readonly wazuh_install_vesion="0.1"
 source_branch="v${wazuh_version}"
