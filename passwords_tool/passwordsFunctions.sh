b5a94f865647291c07c8a77678c8bb31f088ed53
Carlos Anguita López <canguita99@gmail.com>
2024-12-19 14:21:50 +0100

    revert change to trigger the passwords tool

--- passwords_tool/passwordsFunctions.sh
+++ passwords_tool/passwordsFunctions.sh
@@ -5,7 +5,6 @@
 # and/or modify it under the terms of the GNU General Public
 # License (version 2) as published by the FSF - Free Software
 # Foundation.
-# Trigger
 
 function passwords_changePassword() {
 
