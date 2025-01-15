1ac42fd80ab577149b7a87a9265ca38a16a42ec1
Enrique Araque <minixute@gmail.com>
2025-01-15 11:29:08 +0100

    Revert source branch for 4.10.1-rc1

--- install_functions/installVariables.sh
+++ install_functions/installVariables.sh
@@ -11,7 +11,7 @@ readonly wazuh_major="4.10"
 readonly wazuh_version="4.10.1"
 readonly filebeat_version="7.10.2"
 readonly wazuh_install_vesion="0.1"
-source_branch="v${wazuh_version}-rc1"
+source_branch="v${wazuh_version}"
 
 repogpg="https://packages.wazuh.com/key/GPG-KEY-WAZUH"
 repobaseurl="https://packages.wazuh.com/4.x"
