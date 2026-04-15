0d87592b9ea5f794b4cf72bac6a87f0149d0e8fd
fcaffieri <federico.caffieri@wazuh.com>
2026-04-15 11:23:45 -0300

    Remove rename of components names

--- install_functions/installCommon.sh
+++ install_functions/installCommon.sh
@@ -125,12 +125,6 @@ function installCommon_createCertificates() {
 
         common_logger -d "Configuration file downloaded successfully"
 
-        eval "sed -i 's|- name: indexer|- name: wazuh-indexer|' '${config_file}'" ${debug}
-        eval "sed -i 's|ip: \"<indexer-node-ip>\"|ip: \"127.0.0.1\"|' '${config_file}'" ${debug}
-        eval "sed -i 's|- name: manager|- name: wazuh-manager|' '${config_file}'" ${debug}
-        eval "sed -i 's|ip: \"<wazuh-manager-ip>\"|ip: \"127.0.0.1\"|' '${config_file}'" ${debug}
-        eval "sed -i 's|- name: dashboard|- name: wazuh-dashboard|' '${config_file}'" ${debug}
-        eval "sed -i 's|ip: \"<dashboard-node-ip>\"|ip: \"127.0.0.1\"|' '${config_file}'" ${debug}
     fi
 
     cert_readConfig
